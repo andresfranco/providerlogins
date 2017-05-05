c3d1578066825472750ed61f94660e857e187a4f
Andres Franco <koloinotzente@gmail.com>
2017-05-04 20:54:31 -0500

    final course commit

--- web/models/topic.ex
+++ web/models/topic.ex
@@ -2,7 +2,8 @@ defmodule Discuss.Topic do
 use Discuss.Web, :model
   schema "topics" do
     field :title , :string
-    belongs_to :user,Discuss.User 
+    belongs_to :user, Discuss.User
+    has_many :comments , Discuss.Comment
   end
 
   def changeset(struct,params \\ %{} ) do
